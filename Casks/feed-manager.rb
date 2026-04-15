b3f987a4c871cae5fc64f2311aa70d0b128a70fc
lgriffie <loic@griffie.fr>
2026-04-15 13:36:14 +0200

    Initial: add feed-manager cask placeholder

---
+++ Casks/feed-manager.rb
@@ -0,0 +1,21 @@
+cask "feed-manager" do
+    version "0.0.0"
+    sha256 :no_check
+
+    url "https://github.com/Pictarine/feed-manager/releases/download/v#{version}/FeedManager-#{version}.zip"
+    name "Feed Manager"
+    desc "macOS app for managing Firebase Remote Config"
+    homepage "https://github.com/Pictarine/feed-manager"
+
+    depends_on macos: ">= :sonoma"
+
+    app "FeedManager.app"
+
+    zap trash: [
+        "~/Library/Preferences/com.mlg.feed-manager.plist",
+        "~/Library/Preferences/com.pictarine.feed-manager.plist",
+        "~/Library/Application Support/FeedManager",
+        "~/Library/Caches/com.mlg.feed-manager",
+        "~/Library/Caches/com.pictarine.feed-manager",
+    ]
+end
---
+++ README.md
@@ -0,0 +1,12 @@
+# Pictarine Homebrew Tools
+
+  Personal Homebrew tap for Pictarine tools.
+
+  ## Install
+
+    brew tap pictarine/tools
+    brew install feed-manager
+
+  ## Upgrade
+
+    brew upgrade feed-manager
